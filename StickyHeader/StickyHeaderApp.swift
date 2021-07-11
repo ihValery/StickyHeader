8d174b3d28b724d499c58506f9ca642cfb37509a
ihValery <ihValery@gmail.com>
2021-07-11 17:48:15 +0300

    Initial Commit

---
+++ StickyHeader/StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderApp.swift
+//  StickyHeader
+//
+//  Created by Валерий Игнатьев on 11.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
