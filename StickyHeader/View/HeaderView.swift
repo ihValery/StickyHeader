f77e7224a38cd3c5d4babaad6c021d0b0ebc48a9
ihValery <ihValery@gmail.com>
2021-07-11 22:59:36 +0300

    Планов появление кнопки Back

--- StickyHeader/View/HeaderView.swift
+++ StickyHeader/View/HeaderView.swift
@@ -8,31 +8,62 @@
 import SwiftUI
 
 struct HeaderView: View {
+    @EnvironmentObject var homeData: HomeViewModel
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Text("Sticky Header Animation")
-                .font(.title)
-                .fontWeight(.bold)
+        VStack(alignment: .leading, spacing: 0) {
             
-            Text("Chair . Table . Sofa . Buffet")
-                .font(.caption)
-            
-            HStack(spacing: 8) {
-                Image(systemName: "clock")
-                Text("30-40 min")
-                Text("4.3")
-                Image(systemName: "star.fill")
-                Text("$6.30 Free")
-                    .padding(.leading, 10)
+            HStack(spacing: 0) {
+                Button(action: {}, label: {
+                    Image(systemName: "arrow.left")
+                        .font(.title2.bold())
+                        .frame(width: getSize(), height: getSize())
+                        .foregroundColor(.primary)
+                })
+                Text("Sticky Header")
+                    .font(.title)
+                    .fontWeight(.bold)
             }
-            .font(.caption)
-            .frame(maxWidth: .infinity, alignment: .leading)
             
-            Spacer()
+            ZStack {
+                VStack(alignment: .leading, spacing: 10, content: {
+                    Text("Chair . Table . Sofa . Buffet")
+                        .font(.caption)
+                    
+                    HStack(spacing: 8) {
+                        Image(systemName: "clock")
+                        Text("30-40 min")
+                        Text("4.3")
+                        Image(systemName: "star.fill")
+                        Text("$6.30 Free")
+                            .padding(.leading, 10)
+                    }
+                    .font(.caption)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                })
+            }
+            .frame(height: 60)
+            
+            if homeData.offset > 250 {
+                Divider()
+            } else {
+                Divider().hidden()
+            }
         }
         .padding(.horizontal)
         .frame(height: 100)
         .background(Color.white)
+    }
+    
+    //Получение размера кнопки и создание анимации
+    private func getSize() -> CGFloat {
+        if homeData.offset > 200 {
+            let progress = (homeData.offset - 200) / 50
+            
+            return progress <= 1 ? progress * 40 : 40
+        } else {
+            return 0
+        }
     }
 }
 
--- StickyHeader/View/Home.swift
+++ StickyHeader/View/Home.swift
@@ -8,15 +8,32 @@
 import SwiftUI
 
 struct Home: View {
+    @StateObject var homeData = HomeViewModel()
+    
     var body: some View {
         ScrollView {
             LazyVStack(alignment: .leading, spacing: 15, pinnedViews: [.sectionHeaders], content: {
-                //Parallax Header
                 
-                GeometryReader { gr in
-                    Image("StickyHeaderBanner")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
+                //Parallax Header
+                GeometryReader { gr -> AnyView in
+                    let offset = gr.frame(in: .global).minY
+                    
+//                    if -offset >= 0 {
+                    //Что бы спрятать скролл в безопасной зоне
+                        DispatchQueue.main.async {
+                            homeData.offset = -offset
+                        }
+//                    }
+                    
+                    return AnyView(
+                        Image("StickyHeaderBanner")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width,
+                                   height: 250 + (offset > 0 ? offset : 0))
+                            .cornerRadius(1)  //Тут для обрезки
+                            .offset(y: offset > 0 ? -offset : 0)
+                    )
                 }
                 .frame(height: 250)
                 
@@ -46,7 +63,10 @@ struct Home: View {
             Color.white
                 .frame(height: UIApplication.shared.windows.first?.safeAreaInsets.top)
                 .ignoresSafeArea(.all, edges: .top)
+                .opacity(homeData.offset > 250 ? 1 : 0)
             , alignment: .top)
+        //Использовать объект среды для доступа ко всем под объектам
+        .environmentObject(homeData)
     }
 }
 
