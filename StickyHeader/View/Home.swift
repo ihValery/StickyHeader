a4553ea2781e460bb93769c53793157dfa41fa4d
ihValery <ihValery@gmail.com>
2021-07-12 23:15:09 +0300

    Finish

--- StickyHeader/View/Home.swift
+++ StickyHeader/View/Home.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Home: View {
     @ObservedObject var viewModel: ViewModel
-    @Environment(\.colorScheme) var scheme
+    @Environment(\.colorScheme) private var scheme
     
     var body: some View {
         ScrollView {
@@ -21,37 +21,7 @@ struct Home: View {
                 //Card
                 Section(header: HeaderView(viewModel: viewModel)) {
                     
-                    ForEach(tabItems) { item in
-                        VStack(alignment: .leading, spacing: 15, content: {
-                            
-                            Text(item.name.rawValue.firstCapitalized)
-                                .font(.title2.bold())
-                                .foregroundColor(.purpleApp)
-                                .padding(.leading, 20)
-                                .padding(.bottom)
-                            
-                            ForEach(item.furnitures) { furniture in
-                                CardView(furniture: furniture)
-                            }
-                            
-                            Divider()
-                                .padding(.top)
-                        })
-                        .tag(item.name)
-                        .overlay(
-                            GeometryReader { gr -> Text in
-                                //Расчет для определения какая вкладка
-                                let offset = gr.frame(in: .global).minY
-                                let height = UIApplication.shared.windows.first!.safeAreaInsets.top + 100 //+ height Header
-                                if offset < height && offset > 50 && viewModel.selectTab != item.name {
-                                    DispatchQueue.main.async {
-                                        viewModel.selectTab = item.name
-                                    }
-                                }
-                                    
-                                return Text("")
-                            })
-                    }
+                    BodyWithContex(viewModel: viewModel)
                 }
             })
         }
--- StickyHeader/ViewModel/ViewModel.swift
+++ StickyHeader/ViewModel/ViewModel.swift
@@ -9,13 +9,6 @@ import SwiftUI
 import Combine
 
 class ViewModel: ObservableObject {
-//    var localDataFetcher = LocalDataFetcher()
-    
     @Published var offset: CGFloat = 0
     @Published var selectTab = tabItems.first!.name
-//    @Published var furnitures: [Furniture]
-//
-//    init() {
-//        self.furnitures = localDataFetcher.load("furnitureData.json")
-//    }
 }
